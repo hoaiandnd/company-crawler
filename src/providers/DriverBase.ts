c38b6bdb532d9e2ba1940813fd478199b81811cc
HoaiAnZorin <ngohoaian01031@gmail.com>
2026-05-24 01:35:16 +0700

    update

--- src/providers/DriverBase.ts
+++ src/providers/DriverBase.ts
@@ -4,10 +4,10 @@ import { CrawlResult, DriverComponent, ExportOptions } from '@/types/driver.js'
 import { getDomainFromUrl } from '@/utils/extractor.js'
 import { waitRandom } from '@/utils/function.js'
 import { DriverContext } from '@/providers/DriverContext.js'
+import { ErrorMessage } from '@/constants/error.js'
 
 export class DriverBase<TCrawlData extends { phone: string }, TFetchOptions = RequestInit> {
   protected readonly _components: DriverComponent<TCrawlData, TFetchOptions>
-
   // chứa các thông tin sẽ được truyền cho các thành phần của driver
   protected readonly _context: DriverContext
 
@@ -19,10 +19,15 @@ export class DriverBase<TCrawlData extends { phone: string }, TFetchOptions = Re
     const DEFAULT_CRAWL_LIMIT = 5
     const limiter = pLimit(this._context?.driverConfig?.crawlLimit ?? DEFAULT_CRAWL_LIMIT)
     const fetchAndCrawl = async (url: string) => {
-      const html = await this._components.fetcher.fetch(url)
-      const companyDetail = await this._components.crawler.crawl(html, this._context?.driverConfig)
-      const isValid = await this._components.validator?.validate(this._context, companyDetail)
-      return isValid ? companyDetail : null
+      try {
+        const html = await this._components.fetcher.fetch(url)
+        const companyDetail = await this._components.crawler.crawl(html, this._context?.driverConfig)
+        const isValid = await this._components.validator?.validate(this._context, companyDetail)
+        return isValid ? companyDetail : null
+      } catch {
+        console.log(`CANNOT FETCH '${url}'`)
+        return null
+      }
     }
     const fetchQueue = companyLinks.map(url => limiter(() => fetchAndCrawl(url)))
     return Promise.all(fetchQueue)
@@ -30,18 +35,22 @@ export class DriverBase<TCrawlData extends { phone: string }, TFetchOptions = Re
   protected async _run(exportOptions?: ExportOptions<TCrawlData>): Promise<CrawlResult> {
     // sử dụng các thành phần đề tạo thành một chức năng hoàn chỉnh
     const { paginator, fetcher, crawler, exporters } = this._components
-    const { url, filters } = this._context?.crawlRequest
+    const { url, filters, exportFormat } = this._context.crawlRequest
+    const { selectors, crawlLimit } = this._context.driverConfig
     const crawlDomain = getDomainFromUrl(url)
+    if (!crawlDomain) {
+      throw new Error(ErrorMessage.ERR_INVALID_REQUEST)
+    }
     // load and save driver configurations
-    let crawlLimit = filters?.limit && filters.limit > 0 ? filters.limit : this._context.driverConfig.crawlLimit
+    let limit = filters?.limit && filters.limit > 0 ? filters.limit : crawlLimit
     const page = await paginator.paginate(this._context)
-    const format = this._context.crawlRequest.exportFormat ?? 'csv'
+    const format = exportFormat ?? 'csv'
     const exportedFileName = exportOptions?.fileName ?? `${crawlDomain}_${Date.now()}.${format.replace(/^\./, '')}`
-    while (crawlLimit >= 0) {
+    while (limit >= 0) {
       await waitRandom()
 
       const html = await fetcher.fetch(page.current.url)
-      const companyLinks = await crawler.crawlLinks(html, this._context.driverConfig?.selectors?.companyLinks ?? '')
+      const companyLinks = await crawler.crawlLinks(html, selectors?.companyLinks ?? '')
 
       const companies = await this._setFetchQueue(companyLinks)
 
@@ -59,9 +68,9 @@ export class DriverBase<TCrawlData extends { phone: string }, TFetchOptions = Re
       }
       companies.length = 0 // giải phóng bộ nhớ cho mảng sau khi xuất dữ liệu
       await page.goNext()
-      crawlLimit--
+      limit--
     }
-    if (crawlLimit < 0) {
+    if (limit < 0) {
       return {
         isFinish: true,
         lastPage: page.current.page,
