6bc1ba68da8f2a3208e26b373d07f552bf32c772
ngohoaian <ngohoaian01031@gmail.com>
2026-05-26 09:15:17 +0700

    update

--- src/drivers/hoptackinhdoanh.com/DriverFetcher.ts
+++ src/drivers/hoptackinhdoanh.com/DriverFetcher.ts
@@ -1,7 +1,7 @@
-import { IDriverFetcher } from "@/types/driver.js";
+import { IDriverFetcher } from '@/types/driver.js'
 
 export class DriverFetcher implements IDriverFetcher {
   fetch(url: string, options?: RequestInit | undefined): string | Promise<string> {
-    throw new Error("Method not implemented.");
+    return fetch(url, options).then(res => res.text())
   }
 }
