280444aa7ab64bc933cf7726e961c6e6ed66288a
ngohoaian <ngohoaian01031@gmail.com>
2026-05-28 23:53:54 +0700

    update

--- src/drivers/hoptackinhdoanh.com/DriverCrawler.ts
+++ src/drivers/hoptackinhdoanh.com/DriverCrawler.ts
@@ -1,8 +1,12 @@
 import { CompanyDetail } from '@/types/common.js'
 import { DriverConfig, IDriverCrawler } from '@/types/driver.js'
+import * as cheerio from 'cheerio'
 
 export class DriverCrawler implements IDriverCrawler<CompanyDetail> {
   crawlLinks(html: string, selector: string): string[] | Promise<string[]> {
+    if(html === '') throw new Error('ERR_HTML_EMPTY')
+    if(selector === '') throw new Error('ERR_SELECTOR_EMPTY')
+    const $ = cheerio
     throw new Error('Method not implemented.')
   }
   crawl(html: string, config: DriverConfig): CompanyDetail | Promise<CompanyDetail> {
