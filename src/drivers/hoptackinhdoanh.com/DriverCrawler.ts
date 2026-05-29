bcce5e52fded6761f425b6e12f675d3bc90f719f
ngohoaian <ngohoaian01031@gmail.com>
2026-05-29 13:41:46 +0700

    update

--- src/drivers/hoptackinhdoanh.com/DriverCrawler.ts
+++ src/drivers/hoptackinhdoanh.com/DriverCrawler.ts
@@ -3,13 +3,28 @@ import { DriverConfig, IDriverCrawler } from '@/types/driver.js'
 import * as cheerio from 'cheerio'
 
 export class DriverCrawler implements IDriverCrawler<CompanyDetail> {
+  getCompanyDetail(selector: DriverConfig['selectors'], html?: string) {
+    const selectors = selector?.companyDetail
+  }
   crawlLinks(html: string, selector: string): string[] | Promise<string[]> {
-    if(html === '') throw new Error('ERR_HTML_EMPTY')
-    if(selector === '') throw new Error('ERR_SELECTOR_EMPTY')
-    const $ = cheerio
-    throw new Error('Method not implemented.')
+    if (html === '') throw new Error('ERR_HTML_EMPTY')
+    if (selector === '') throw new Error('ERR_SELECTOR_EMPTY')
+    const $ = cheerio.load(html)
+    const links = $(selector)
+      .map((i, el) => $(el).attr('href'))
+      .get()
+    return links
   }
   crawl(html: string, config: DriverConfig): CompanyDetail | Promise<CompanyDetail> {
+    const selectors = config.selectors?.companyDetail
+    const $ = cheerio.load(html)
+    for (const key in selectors) {
+      const selector = selectors?.[key as keyof typeof selectors]
+      const value = $(selector).text().trim()
+      if(value) {
+
+      }
+    }
     throw new Error('Method not implemented.')
   }
 }
