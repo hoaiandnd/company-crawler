4fb91f33315c89350fcc186edc8a1fd9bb1772bb
HoaiAnZorin <ngohoaian01031@gmail.com>
2026-05-31 20:33:24 +0700

    update

---
+++ src/drivers/hoptackinhdoanh.com/DriverValidator.ts
@@ -0,0 +1,16 @@
+import { CompanyDetail } from '@/types/common.js'
+import { DriverContext, IDriverValidator } from '@/types/driver.js'
+
+export class DriverValidator implements IDriverValidator<CompanyDetail> {
+  validate(
+    _context: DriverContext,
+    data: CompanyDetail
+  ): boolean | Promise<boolean> {
+    if (!data) return false
+    const { phone } = data
+    if (!phone) return false
+    const phoneRegex =
+      /^(0|\+84)(\s|\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\d)(\s|\.)?(\d{3})(\s|\.)?(\d{3})$/
+    return phoneRegex.test(phone)
+  }
+}
